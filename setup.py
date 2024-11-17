9ee3d0acfe3ca10adf6e100b8d8730d2e971c88c
dhruv <dhruvsaini2209@gmail.com>
2024-11-17 12:19:06 +0530

    Second commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def  get_requirements(file_path:str)->List[str]:
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]0
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+name='ml_project',
+version='0.0.1',
+author='Dhruv'
+author_email='Dhruvsaini2209@gmail.com'
+packages=find_packages(),
+install_require=get_requirements('requirement.txt')
+)
